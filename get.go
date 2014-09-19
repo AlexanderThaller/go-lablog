1606c5af6bc879bdd31d9472c6ad2a4f79ac2362
Alexander Thaller <alexander.thaller@atraveo.de>
2014-09-19 12:56:36 +0200

    will not print dotfiles as projects anymore.

--- get.go
+++ get.go
@@ -15,6 +15,13 @@ func GetProjects(datapath string) ([]string, error) {
 	var out []string
 	for _, d := range dir {
 		file := d.Name()
+		l.Debug("DirName: ", file)
+
+		if strings.HasPrefix(file, ".") {
+			l.Trace(file, " has prefix '.'")
+			continue
+		}
+
 		ext := filepath.Ext(file)
 		name := file[0 : len(file)-len(ext)]
 
