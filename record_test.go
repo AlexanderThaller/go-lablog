bd857e79575a89cbbc7cf5b881f3da44aea9e42f
Alexander Thaller <alexander@thaller.ws>
2014-10-06 21:47:27 +0200

    added test for Todo.CSV.

--- record_test.go
+++ record_test.go
@@ -10,12 +10,10 @@ import (
 func Test_NoteCSV(t *testing.T) {
 	l := logger.New(Name, "Test", "Note", "CSV")
 
-	project := "TestProject"
-	timestamp := time.Time{}
-	value := "TestValue"
+	timestamp := time.Date(2009, time.November, 10, 23, 1, 2, 3, time.UTC)
+	value := "NoteTestValue"
 
 	note := Note{
-		Project:   project,
 		TimeStamp: timestamp,
 		Value:     value,
 	}
@@ -32,7 +30,7 @@ func Test_NoteCSV(t *testing.T) {
 	{
 		message := "TimeStamp of note does not match timestamp of csv"
 		got := out[0]
-		expected := "0001-01-01T00:00:00Z"
+		expected := timestamp.Format(time.RFC3339Nano)
 		test(t, l, message, got, expected)
 	}
 
@@ -46,7 +44,58 @@ func Test_NoteCSV(t *testing.T) {
 	{
 		message := "Value of note does not match note of csv"
 		got := out[2]
-		expected := "TestValue"
+		expected := value
+		test(t, l, message, got, expected)
+	}
+}
+
+func Test_TodoCSV(t *testing.T) {
+	l := logger.New(Name, "Test", "Todo", "CSV")
+
+	timestamp := time.Date(2009, time.November, 10, 23, 1, 2, 3, time.UTC)
+	value := "TodoTestValue"
+	done := true
+
+	todo := Todo{
+		TimeStamp: timestamp,
+		Value:     value,
+		Done:      done,
+	}
+
+	out := todo.CSV()
+
+	{
+		message := "Length of csv for todo is wrong"
+		got := len(out)
+		expected := 4
+		test(t, l, message, got, expected)
+	}
+
+	{
+		message := "TimeStamp of todo does not match timestamp of csv"
+		got := out[0]
+		expected := timestamp.Format(time.RFC3339Nano)
+		test(t, l, message, got, expected)
+	}
+
+	{
+		message := "Action of output csv is not todo"
+		got := out[1]
+		expected := "todo"
+		test(t, l, message, got, expected)
+	}
+
+	{
+		message := "Value of todo does not match todo of csv"
+		got := out[2]
+		expected := value
+		test(t, l, message, got, expected)
+	}
+
+	{
+		message := "Done of todo does not match todo of csv"
+		got := out[3]
+		expected := "true"
 		test(t, l, message, got, expected)
 	}
 }
