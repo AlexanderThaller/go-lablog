a5db6a58e8e24e296b5a063d32d034dd39e8330a
Alexander Thaller <alexander.thaller@trivago.com>
2015-09-14 13:45:22 +0200

    Added loglevel flag.

--- src/commands/main.go
+++ src/commands/main.go
@@ -13,22 +13,24 @@ import (
 )
 
 var flagDataDir string
+var flagLogLevel string
 
 func init() {
-	log.SetLevel(log.DebugLevel)
-
 	homepath, err := homedir.Dir()
 	helper.ErrExit(errgo.Notef(err, "can not get homepath"))
 
 	datadir := path.Join(homepath, ".lablog")
 	cmdMain.PersistentFlags().StringVarP(&flagDataDir, "datadir", "d",
 		datadir, "The path to the datadir for retreiving and storing the data.")
+	cmdMain.PersistentFlags().StringVarP(&flagLogLevel, "loglevel", "l",
+		"warn", "The loglevel for which to run in. Default is warn. There are panic, fatal, error, warn info and debug as levels.")
 }
 
 var cmdMain = &cobra.Command{
-	Use:   "lablog [command]",
-	Short: "lablog makes taking notes and todos easy.",
-	Long:  `lablog orders notes and todos into projects and subprojects without dictating a specific format.`,
+	Use:              "lablog [command]",
+	Short:            "lablog makes taking notes and todos easy.",
+	Long:             `lablog orders notes and todos into projects and subprojects without dictating a specific format.`,
+	PersistentPreRun: setLogLevel,
 }
 
 func Run() {
@@ -50,4 +52,13 @@ func Run() {
 	cmdAddTodo.AddCommand(cmdAddTodoInActive)
 
 	cmdMain.Execute()
+}
+
+func setLogLevel(cmd *cobra.Command, args []string) {
+	level, err := log.ParseLevel(flagLogLevel)
+	if err != nil {
+		log.Fatal(errgo.Notef(err, "can not parse loglevel from flag"))
+	}
+
+	log.SetLevel(level)
 }
