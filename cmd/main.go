43eef9ca5299f94735475e61d7fa1ae73c257cbb
Alexander Thaller <alexander.thaller@trivago.com>
2016-05-11 02:13:50 +0200

    Layout for ncurses interface.

--- cmd/main.go
+++ cmd/main.go
@@ -24,6 +24,7 @@ import (
 	"path"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/jroimartin/gocui"
 
 	"github.com/AlexanderThaller/lablog/src/helper"
 
@@ -53,8 +54,54 @@ var RootCmd = &cobra.Command{
 	Use:               "lablog [command]",
 	Short:             "lablog makes taking notes and todos easy",
 	Long:              `lablog orders notes and todos into projects and subprojects without dictating a specific format.`,
-	RunE:              runCmdShowProjects,
+	RunE:              runCmdRoot,
 	PersistentPreRunE: setLogLevel,
+}
+
+const (
+	HEIGHT = 10
+	WIDTH  = 40
+)
+
+func runCmdRoot(cmd *cobra.Command, args []string) error {
+	g := gocui.NewGui()
+	if err := g.Init(); err != nil {
+		return err
+	}
+	defer g.Close()
+
+	g.SetLayout(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		return err
+	}
+
+	return nil
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if _, err := g.SetView("side", -1, -1, int(0.2*float32(maxX)), maxY-5); err != nil &&
+		err != gocui.ErrUnknownView {
+		return err
+	}
+	if _, err := g.SetView("main", int(0.2*float32(maxX)), -1, maxX, maxY-5); err != nil &&
+		err != gocui.ErrUnknownView {
+		return err
+	}
+	if _, err := g.SetView("cmdline", -1, maxY-5, maxX, maxY); err != nil &&
+		err != gocui.ErrUnknownView {
+		return err
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
 
 func Execute() {
